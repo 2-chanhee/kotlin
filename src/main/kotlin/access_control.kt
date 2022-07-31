ce4470efacdb3f40148f23df12b93a9cb19d67f7
2-chanhee <l01083582454@gmail.com>
2022-07-31 19:22:07 +0900

    class 10 access control (접근 제어)

--- src/main/kotlin/access_control.kt
+++ src/main/kotlin/access_control.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
 
 }
 
+// class 10
 // private constructor를 선언하려면 constructor 앞에 private 사용해야 함
 class Car private constructor(
     // protected: 선언된 클래스 or 하위 클래스에서만 접근 가능
